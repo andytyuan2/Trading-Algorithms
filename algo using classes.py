7876aff48bb2edceadd0a87f1fd5224974c8b3ab
Andy Yuan <andy.yuan2tdsb@gmail.com>
2023-10-06 22:32:30 -0400

    Update algo using classes.py

--- algo using classes.py
+++ algo using classes.py
@@ -11,36 +11,34 @@ class stock:
         self.ticker = ticker
         
     def stocks(self):
-        stock = yf.Ticker(self.ticker)
-        historical_price_stock = np.array(round(stock.history(start="2022-06-06", end="2023-06-06", interval="1d")['Close'], 6))
+        stock_of_choice = yf.Ticker(self.ticker)
+        historical_price_stock = list(round(stock_of_choice.history(start="2022-06-06", end="2022-06-14", interval="1d")['Close'], 3))
         return historical_price_stock
     
     def price_change(self):
         price_changes_list = []
-        for x in range(0, len(self.stocks)-1):
-            percent_change = (self.stocks[x+1] - self.stocks[x]) / self.stocks[x]
+        for x in range(0, len(self.stocks())-1):
+            percent_change = round((self.stocks()[x+1] - self.stocks()[x]) * 100 / self.stocks()[x], 3)
             if percent_change == 0:
-                price_changes_list.append(0.000001)
+                price_changes_list.append(0.0001)
             else:
-                price_changes_list.append(float(percent_change))
-        historical_price_change_array = np.array(price_changes_list)
-        return historical_price_change_array
+                price_changes_list.append(percent_change)
+        return price_changes_list
     
-    def average(self):
-        return np.average(self.stocks)
+    def average_price(self):
+        return np.average(self.stocks())
     
     def average_change(self):
-        return np.average(self.price_change)
+        return np.average(self.price_change())
     
     def line_equation(self):
-        print ('The equation of the line of best fit is: y = ', self.average_change, "x + ", self.average)
+        return f'The equation of the line of best fit is: y = {self.average_change()}x + {self.average_price()}'
     
-    def price_plot(self):
+    def price_average_plot(self):
         price_plotted = []
-        for x in range(0, len(self.price_change)):
-            price_plotted.append(self.average + x*self.average_change)
+        for x in range(0, len(self.price_change())):
+            price_plotted.append(self.average_price() + x*self.average_change())
         return price_plotted
 
-p1 = stock("AAPL") 
-print(p1.average())
-print('The equation of the line of best fit is: y = ', p1.average_change(), "x + ", p1.average())
+
+
